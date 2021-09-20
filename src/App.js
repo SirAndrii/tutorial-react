e23f1244773d904cca9c123cb0c70ba553bce97b
Andrii Sirotenko <andruhe@gmail.com>
2021-09-20 11:40:00 +0300

    added loading data from fetch request

--- src/App.js
+++ src/App.js
@@ -3,30 +3,40 @@ import React from 'react'; // подключение библиотеки React
 import './App.css'; // подключение файла стилей
 import { Add } from './components/Add'
 import { News } from './components/News'
-import newsData from './data/newsData'
-
-
+//import newsData from './data/newsData'
 
 
 class App extends React.Component {
   state = {
-    news: newsData
+    //news: newsData
+    news: null,
+    isLoading: false, // статус для манипуляций "прелоадером" 
   };
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch('http://localhost:3000/data/newsData.json')
+      .then(response => response.json())
+      .then((data)=> this.setState({ isLoading: false, news: data }));
+  }
+
   handleAddNews = data => {
     const nextNews = [data, ...this.state.news];
     this.setState({ news: nextNews });
   };
   render() {
+    const { news, isLoading } = this.state;
+
     return (
       <React.Fragment>
         <Add onAddNews={this.handleAddNews} />
         <h3>Новости</h3>
-        <News data={this.state.news} />
+        {isLoading && <p>Loadding... </p>}
+        {Array.isArray(news) && <News data={ news } />}
       </React.Fragment>
     );
   }
 }
 
 // скопировано все кроме ReactDOM.render
-
 export default App;
